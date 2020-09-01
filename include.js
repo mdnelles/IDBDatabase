f63358597086683ae1c28d4fcfc16657c380330f
MD Nelles <mxnelles@gmail.com>
2020-09-01 15:47:37 -0400

    Create include.js

---
+++ include.js
@@ -0,0 +1,43 @@
+const dbPromise = createIndexedDB('masterDB');
+
+function createIndexedDB(dbName) {
+   if (!("indexedDB" in window)) {
+      return null;
+   }
+   //JSMART db created browserDB
+   return idb.open(dbName, 1, function (upgradeDb) {
+      if (!upgradeDb.objectStoreNames.contains(dbName)) {
+         const eventsOS = upgradeDb.createObjectStore("events", {
+            keyPath: "id",
+         });
+      }
+   });
+}
+
+
+// add data
+function saveDataLocally(data,dataHandle) {
+   if (!("indexedDB" in window)) {
+      return null;
+   }
+   return dbPromise.then((db) => {
+      const tx = db.transaction(dataHandle, "readwrite");
+      const store = tx.objectStore(dataHandle);
+      return Promise.all(data.map((adata) => store.put(adata))).catch(() => {
+         tx.abort();
+         throw Error("Events were not added to the store");
+      });
+   });
+}
+
+// retrieve data
+function getLocalData(dataHandle) {
+   if (!("indexedDB" in window)) {
+      return null;
+   }
+   return dbPromise.then((db) => {
+      const tx = db.transaction(dataHandle, "readonly");
+      const store = tx.objectStore("events");
+      return store.getAll();
+   });
+}
